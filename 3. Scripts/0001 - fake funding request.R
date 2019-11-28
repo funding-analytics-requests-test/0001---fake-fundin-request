0cc8901b87c6fbdb62a38cb13ab0ebe2ff3d4fe2
Brown <W.Matthew.Brown@cihr-irsc.gc.ca>
2019-11-28 06:41:55 -0500

    quick fix to script. blah blah

--- 3. Scripts/0001 - fake funding request.R
+++ 3. Scripts/0001 - fake funding request.R
@@ -4,12 +4,11 @@
 # Folder: S:\Swap\...\path\to\project
 
 ### REQUEST ----
-# Here is the information for the new request I mentioned at the team meeting. Lisa Lemieux in Major Initiatives is looking for a list of funded clinical trials over the last two years (2017/18-2018/19) in order to inform the next phase of the SPOR (Strategy for Patient-Oriented Research Clinical Trial Initiative). She would like to scope in project with the clinical trial flag as well as projects that have gone to the RC1 Peer Review Committee. Please include the following fields:
-# •	Project title
-# •	NPI
-# •	Institution (I will clarify whether she is looking for PRI, RIN, or INP) 
-# •	Keywords
-# •	Funding amount 
+# Here is the information for the new request I mentioned at the team meeting. 
+# ......
+# ......
+# ......
+# ......
 
 
 library(mattfun) # some functions for exploring data, not necessary to actually
@@ -26,3 +25,5 @@ loadListed(c("fundTomb"))
 loadListed(c("appKey"))
 
 # and continue on
+
+# added a new line to test git commit stuff
